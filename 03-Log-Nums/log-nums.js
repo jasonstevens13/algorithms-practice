621f276b6f6bd8a9b111b6219406fa0e74feff61
jasonstevens13 <jasonstevens13@yahoo.com>
2020-08-28 15:31:57 -0400

    num log algo added and completed

---
+++ 03-Log-Nums/log-nums.js
@@ -0,0 +1,11 @@
+// Write code to print all numbers from 1 to `num`
+// Assume `num` will be a positive number
+
+// I used a for loop here with the number input itself replacing the usual 'array.length' and telling to to iterate that many times.
+
+var logNums = function (num) {
+    for (let i = 1; i <= num; i++) {
+        console.log(i);
+    }
+
+};
