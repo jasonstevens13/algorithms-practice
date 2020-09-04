a979ab1fc1bf9601b61dbf28bf8b5938ab0a8f3c
jasonstevens13 <jasonstevens13@yahoo.com>
2020-09-04 16:10:10 -0400

    solved sum array

--- 06-Sum-Array/sum-array.js
+++ 06-Sum-Array/sum-array.js
@@ -1,3 +1,29 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+const sumArray = function (arr) {
+
+    // I am thinking that I can easily loop through and add the number to a variable; 
+    // however, I wonder if there is a method to do this as well... I think I used reduce method in a project.
+    // Yes reduc works.
+
+    let sum = arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
+
+    return sum;
+};
+
+
+// For notes, material provided the loop approach that follows:
+
+// var sumArray = function(arr) {
+//     var result = 0;
+
+//     for (var i = 0; i < arr.length; i++) {
+//       var currentNumber = arr[i];
+//       result += currentNumber;
+//     }
+
+//     return result;
+//   };
