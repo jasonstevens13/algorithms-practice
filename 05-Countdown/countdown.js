ebfcccb21b4162b9b78e1a4d07bbb765d8201f95
jasonstevens13 <jasonstevens13@yahoo.com>
2020-09-04 15:36:17 -0400

    Countdown algo added and solved

---
+++ 05-Countdown/countdown.js
@@ -0,0 +1,12 @@
+// Write code to print all numbers from `num` down to 1
+// You may assume `num` will be a positive number
+
+var countdown = function (num) {
+
+    // I believe a simple for loop will work: Start at num; subtract 1; keep iterating as long as i is greater/equal to 1.
+    for (let i = num; i >= 1; i--) {
+        console.log(i);
+
+    }
+
+};
