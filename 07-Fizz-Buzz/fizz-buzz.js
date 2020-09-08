fd6ed4b15d29fb96c5b01898d656024d37c6f133
jasonstevens13 <jasonstevens13@yahoo.com>
2020-09-08 15:40:01 -0400

    fizz buzz solved

--- 07-Fizz-Buzz/fizz-buzz.js
+++ 07-Fizz-Buzz/fizz-buzz.js
@@ -6,6 +6,26 @@
 
 var fizzBuzz = function (arr) {
 
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+
+        if (num % 3 == 0 && num % 5 !== 0) {
+            console.log('Fizz');
+        }
+        else if (num % 5 == 0 && num % 3 !== 0) {
+            console.log('Buzz');
+        }
+        else if (num % 3 == 0 && num % 5 == 0) {
+            console.log('Fizz Buzz');
+        }
+        else {
+            console.log(num);
+        }
+
+    }
 
 
 };
+
+// Note: I had this correct, but was returning rather than console.logging, so the test was not passing at first.
+// Furthermore, I see that in a solved solution they have the Fizz Buzz condition as being divisible by 15, rather than using && like I do above.
