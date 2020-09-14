df72cfc9f408c2a13067ac2565d4085852383ead
jasonstevens13 <jasonstevens13@yahoo.com>
2020-09-14 16:02:05 -0400

    factorial algo solved

--- 11-Factorial/factorial.js
+++ 11-Factorial/factorial.js
@@ -1,3 +1,15 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+
+    let factorial = 1;
+    for (let i = 1; i <= num; i++) {
+        factorial = i * factorial;
+    }
+    return factorial;
+
+
+    // This one felt pretty straightforward. I did have to stop and think about whether I wanted to initialize the factorial variable with no value or 1.
+
+
+};
