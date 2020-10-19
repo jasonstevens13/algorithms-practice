bf688014beadd9d7731be22d7c1b4c3b7c0ba50c
jasonstevens13 <jasonstevens13@yahoo.com>
2020-10-19 14:58:29 -0400

    longest word algo solved

--- 14-Longest-Word/longest-word.js
+++ 14-Longest-Word/longest-word.js
@@ -1,3 +1,26 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+var longestWord = function (str) {
+
+    // Take in str and split by spaces to create an array of words
+    // Then taake each word and count chars
+    // push first word to and var 'longestword'for longest word
+    // as I iterate and count chars and each word, do comparison on length of 'longestword'
+    // replace if longer
+    // return 'longestWord' var
+
+    let words = str.split(" ");
+
+    let longestWord = '';
+
+    words.forEach(word => {
+
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    });
+
+
+    return longestWord;
+
+};
