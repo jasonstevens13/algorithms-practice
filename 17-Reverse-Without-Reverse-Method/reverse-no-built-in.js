2ed72274b1be7d347745c155389bbebbf573f315
jasonstevens13 <jasonstevens13@yahoo.com>
2020-10-19 15:37:49 -0400

    cleaner/optimized code

--- 17-Reverse-Without-Reverse-Method/reverse-no-built-in.js
+++ 17-Reverse-Without-Reverse-Method/reverse-no-built-in.js
@@ -4,14 +4,32 @@
 const reverse = function (str) {
 
 
-    let chars = str.split('');
-    let reversedCharsArray = [];
+    // let chars = str.split('');
+    // let reversedCharsArray = [];
 
-    for (let i = chars.length - 1; i >= 0; i--) {
-        const char = chars[i];
-        reversedCharsArray.push(char);
+    // for (let i = chars.length - 1; i >= 0; i--) {
+    //     const char = chars[i];
+    //     reversedCharsArray.push(char);
+    // }
+
+    // let reversedStr = reversedCharsArray.join('');
+    // return reversedStr
+
+
+    // after solving as above, I looked for optimaztion/cleaner code... 
+    // solved doesn't use split method or the empty array as it is not necessary since we need
+    // to reverse the entire string without regard to separate words etc.
+    // cleaner solve below:
+
+    let result = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        let letter = str[i];
+        result += letter;
     }
 
-    let reversedStr = reversedCharsArray.join('');
-    return reversedStr
+    return result;
+
+
+
 };
