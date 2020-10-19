bc1e5c808d8b4c7351a315bd53e2f02748a548a0
jasonstevens13 <jasonstevens13@yahoo.com>
2020-10-19 16:22:52 -0400

    reversed array without using reverse method algo solved.

---
+++ 18-Reverse-In-Place/reverse-in-place.js
@@ -0,0 +1,34 @@
+// Write code to return a new string with all of the elements of array reversed.
+// You may NOT use the built-in reverse method
+
+const reverseInPlace = function (arr) {
+
+    let left = 0;
+    let right = arr.length - 1;
+    let leftPlaceholder;
+    let rightPlaceholder;
+
+    while (left < right) {
+
+        leftPlaceholder = arr[left];
+        rightPlaceholder = arr[right];
+
+        arr[left] = rightPlaceholder;
+        arr[right] = leftPlaceholder;
+
+
+        left++;
+        right--;
+
+    }
+
+    return arr
+
+
+};
+
+
+// [0,1,2,3,4,5,6,7,8]
+//  L                R
+
+// placeholder = 0
