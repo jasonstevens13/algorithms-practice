271bab52637278c83042d7c96c998d2e425ccd41
jasonstevens13 <jasonstevens13@yahoo.com>
2020-10-26 01:00:07 -0400

    camelCase algo solved

--- 23-Camel-Case/camel-case.js
+++ 23-Camel-Case/camel-case.js
@@ -1,3 +1,20 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+const camelCase = function (str) {
+
+    let camel = [];
+    let strLower = str.toLowerCase();
+    let words = strLower.split(" ");
+    camel.push(words[0]);
+
+    for (let i = 1; i < words.length; i++) {
+        let currentWord = words[i].split("");
+        currentWord[0] = currentWord[0].toUpperCase();
+        camel.push(currentWord.join(""))
+
+    }
+
+    return camel.join("");
+
+
+};
