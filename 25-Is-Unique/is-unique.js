dcce47314e822fc7ee443eea6bcbb9d9b74715aa
jasonstevens13 <jasonstevens13@yahoo.com>
2020-11-02 15:32:59 -0500

    is-unique algo solved

--- 25-Is-Unique/is-unique.js
+++ 25-Is-Unique/is-unique.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+const isUnique = function (arr) {
+
+    let obj = {};
+    let result = true;
+
+    arr.forEach(num => {
+
+        if (obj.hasOwnProperty(num)) {
+            result = false
+        } else {
+            obj[num] = 1;
+        }
+
+    });
+
+    return result
+
+};
