4ac89c19733bcef3f569124146102e1ce91cdec5
jasonstevens13 <jasonstevens13@yahoo.com>
2020-11-02 16:19:43 -0500

    is-anagram algo solved

--- 26-Is-Anagram/is-anagram.js
+++ 26-Is-Anagram/is-anagram.js
@@ -1,4 +1,52 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+const isAnagram = function (strA, strB) {
+
+    // first check to see if the strings ar same length
+    if (strA.length !== strB.length) {
+        return false
+    };
+
+    // If same length, continue...then build out an object for each str that tallies the number of times each char occurs
+    let objA = {};
+    let objB = {};
+
+    for (const char of strA) {
+
+        if (objA.hasOwnProperty(char)) {
+            objA[char]++;
+        } else {
+            objA[char] = 1;
+        }
+    };
+
+
+    for (const char of strB) {
+
+        if (objB.hasOwnProperty(char)) {
+            objB[char]++;
+        } else {
+            objB[char] = 1;
+        }
+    };
+
+
+    // Then check to see if the number of object keys are the same, if so move on to comparing them
+    let objAKeys = Object.keys(objA);
+    let objBKeys = Object.keys(objB);
+
+    if (objAKeys.length !== objBKeys.length) {
+        return false
+    };
+
+
+    for (const objKey in objA) {
+        if (objA[objKey] !== objB[objKey]) {
+            return false
+        }
+    }
+
+
+    return true
+};
--- 26-Is-Anagram/test/is-anagram.test.js
+++ 26-Is-Anagram/test/is-anagram.test.js
@@ -1,7 +1,7 @@
 var expect = chai.expect;
 
-describe("isAnagram", function() {
-  it("should return true when given the strings 'abc` and `cba`", function() {
+describe("isAnagram", function () {
+  it("should return true when given the strings 'abc` and `cba`", function () {
     var strA = "abc";
     var strB = "cba";
 
@@ -10,7 +10,7 @@ describe("isAnagram", function() {
     expect(result).to.eql(true);
   });
 
-  it("should return true when given the strings 'octagonal` and `tganoalc`", function() {
+  it("should return true when given the strings 'octagonal` and `tganoalc`", function () {
     var strA = "octagonal";
     var strB = "tganoalco";
 
@@ -19,7 +19,7 @@ describe("isAnagram", function() {
     expect(result).to.eql(true);
   });
 
-  it("should return false when false the strings 'obsidian` and `danisboba`", function() {
+  it("should return false when false the strings 'obsidian` and `danisboba`", function () {
     var strA = "obsidian";
     var strB = "danisboba";
 
@@ -28,7 +28,7 @@ describe("isAnagram", function() {
     expect(result).to.eql(false);
   });
 
-  it("should return false when false the strings 'targaryen` and `gratawyen`", function() {
+  it("should return false when false the strings 'targaryen` and `gratawyen`", function () {
     var strA = "targaryen";
     var strB = "gratawyen";
 
